73a7eec5cd9e5013f36e65b6c8ef3e553e6f0dee
Marcin <marcin.adam.mroczko@gmail.com>
2021-03-24 23:24:37 +0100

    back btn

--- src/components/SelectLang.js
+++ src/components/SelectLang.js
@@ -11,14 +11,19 @@ import Box from "./Box.js"
 
 function SelectLang() {
   return (
-    <div className="container">
-      <Link className="test" to="/">
-        <Box icon={england} alt="england-flag" title="English" paragraph="English" />
-        <Box icon={german} alt="german-flag" title="Deutsch" paragraph="German" />
-        <Box icon={france} alt="england-flag" title="Français" paragraph="French" />
-        <Box icon={spain} alt="england-flag" title="Español" paragraph="Spanish" />
+    <>
+      <div className="container">
+        <Link className="test" to="/">
+          <Box icon={england} alt="england-flag" title="English" paragraph="English" />
+          <Box icon={german} alt="german-flag" title="Deutsch" paragraph="German" />
+          <Box icon={france} alt="england-flag" title="Français" paragraph="French" />
+          <Box icon={spain} alt="england-flag" title="Español" paragraph="Spanish" />
+        </Link>
+      </div>
+      <Link className="btn" to="/">
+        Back
       </Link>
-    </div>
+    </>
   )
 }
 
