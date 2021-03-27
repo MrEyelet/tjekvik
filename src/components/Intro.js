85976630dc1047eeaa059b69cfd86a20e4317088
Marcin <marcin.adam.mroczko@gmail.com>
2021-03-28 00:27:04 +0100

    gsap animation

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,16 +4,12 @@ import gsap from "gsap"
 function Intro(props) {
   useEffect(() => {
     const tl = gsap.timeline({ defaults: { ease: "Power3.inOut" } })
-    tl.to(".gsap-appear-title", { duration: 0.75, y: "0", stagger: 0.2 })
+    tl.to(".gsap-appear-title", { duration: 0.75, y: "0", opacity: 1, stagger: 0.2 })
   })
   return (
     <section className="intro">
-      <h1 className="intro__title">
-        <span className="intro__title-inner  gsap-appear-title">{props.title}</span>
-      </h1>
-      <h2 className="intro__subtitle">
-        <span className="intro__subtitle-inner  gsap-appear-title">{props.subtitle}</span>
-      </h2>
+      <h1 className="intro__title gsap-appear-title">{props.title}</h1>
+      <h2 className="intro__subtitle gsap-appear-title">{props.subtitle}</h2>
     </section>
   )
 }
