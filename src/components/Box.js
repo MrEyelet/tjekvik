858fcb67285d10f71bec7dfde9979d1975a8073a
Marcin <marcin.adam.mroczko@gmail.com>
2021-03-27 19:06:10 +0100

    import svg as react component

--- src/components/Box.js
+++ src/components/Box.js
@@ -3,9 +3,7 @@ import React from "react"
 function Box(props) {
   return (
     <div className="box">
-      <div className="box__icon">
-        <img src={props.icon} alt={props.alt} />
-      </div>
+      <div className="box__icon">{props.icon}</div>
 
       <h3 className="box__title">{props.title}</h3>
       <p className="box__desc">{props.paragraph}</p>
--- src/components/ServiceOptions.js
+++ src/components/ServiceOptions.js
@@ -3,21 +3,21 @@ import { Link } from "react-router-dom"
 //Components
 import Box from "./Box.js"
 //Graphics
-import checkInIcon from "../assets/check-in-icon.svg"
-import checkOutIcon from "../assets/check-out-icon.svg"
-import noAppointmentIcon from "../assets/no-appointment-icon.svg"
+import { ReactComponent as CheckInIcon } from "../assets/check-in-icon.svg"
+import { ReactComponent as CheckOutIcon } from "../assets/check-out-icon.svg"
+import { ReactComponent as NoAppointmentIcon } from "../assets/no-appointment-icon.svg"
 
 function ServiceOptions(props) {
   return (
     <>
       <Link className="link" to="/">
-        <Box icon={checkInIcon} alt="check-in-icon" title="check in" paragraph={props.checkIn} />
+        <Box icon={<CheckInIcon />} alt="check-in-icon" title="check in" />
       </Link>
       <Link className="link" to="/">
-        <Box icon={checkOutIcon} alt="check-out-icon" title="check out" paragraph={props.checkOut} />
+        <Box icon={<CheckOutIcon />} alt="check-out-icon" title="check out" paragraph={props.checkOut} />
       </Link>
       <Link className="link" to="/">
-        <Box icon={noAppointmentIcon} alt="no-appointment-icon" title={props.title} paragraph={props.withoutCheck} />
+        <Box icon={<NoAppointmentIcon />} alt="no-appointment-icon" title={props.title} paragraph={props.withoutCheck} />
       </Link>
     </>
   )
