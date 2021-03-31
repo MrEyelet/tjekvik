c156b5600c8d8afe0ae08a854381ce5866d21e43
Marcin <marcin.adam.mroczko@gmail.com>
2021-03-31 15:17:10 +0200

    back to top btn

--- src/components/Btn.js
+++ src/components/Btn.js
@@ -2,8 +2,11 @@ import React from "react"
 import { Link } from "react-router-dom"
 
 function Btn(props) {
+  function scrolltop() {
+    window.scrollTo(0, 0)
+  }
   return (
-    <Link className="btn" to="/">
+    <Link className="btn" to="/" onClick={scrolltop}>
       {props.text}
     </Link>
   )
