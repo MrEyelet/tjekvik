d46a7e70ee94e53b3a1f933298f1010e2af1c42a
Marcin <marcin.adam.mroczko@gmail.com>
2021-03-31 19:48:19 +0200

    select lang subpage

--- src/components/SelectLang.js
+++ src/components/SelectLang.js
@@ -13,48 +13,56 @@ import { ReactComponent as Spain } from "../assets/spain.svg"
 
 function SelectLang() {
   return (
-    <main className="select-lang">
+    <>
       {(() => {
         const body = document.querySelector("body")
         if (body.classList.contains("en")) {
           return (
             <>
               <Header language="en" cta="Need assistance" flag={<England />} />
-              <Intro title="Select language" />
-              <LanguageOptions />
-              <Btn text="Back" />
+              <main className="select-lang">
+                <Intro title="Select language" />
+                <LanguageOptions />
+                <Btn text="Back" />
+              </main>
             </>
           )
         } else if (body.classList.contains("de")) {
           return (
             <>
               <Header language="de" cta="Brauche Hilfe" flag={<German />} />
-              <Intro title="Sprache auswählen" />
-              <LanguageOptions />
-              <Btn text="Abbrechen" />
+              <main className="select-lang">
+                <Intro title="Sprache auswählen" />
+                <LanguageOptions />
+                <Btn text="Abbrechen" />
+              </main>
             </>
           )
         } else if (body.classList.contains("fr")) {
           return (
             <>
               <Header language="fr" cta="Besoin d'assistance" flag={<France />} />
-              <Intro title="Choisir la langue" />
-              <LanguageOptions />
-              <Btn text="Retourner" />
+              <main className="select-lang">
+                <Intro title="Choisir la langue" />
+                <LanguageOptions />
+                <Btn text="Retourner" />
+              </main>
             </>
           )
         } else {
           return (
             <>
               <Header language="es" cta="Necesita ayuda" flag={<Spain />} />
-              <Intro title="Seleccione el idioma" />
-              <LanguageOptions />
-              <Btn text="Regresa" />
+              <main className="select-lang">
+                <Intro title="Seleccione el idioma" />
+                <LanguageOptions />
+                <Btn text="Regresa" />
+              </main>
             </>
           )
         }
       })()}
-    </main>
+    </>
   )
 }
 
