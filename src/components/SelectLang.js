3be5819eb9d07587cc3aa759c1d52c0e07a5f5db
Marcin <marcin.adam.mroczko@gmail.com>
2021-03-31 16:58:33 +0200

    labguage title change

--- src/components/SelectLang.js
+++ src/components/SelectLang.js
@@ -13,36 +13,48 @@ import { ReactComponent as Spain } from "../assets/spain.svg"
 
 function SelectLang() {
   return (
-    <>
+    <main className="select-lang">
       {(() => {
         const body = document.querySelector("body")
         if (body.classList.contains("en")) {
-          return <Header language="en" cta="Need assistance" flag={<England />} />
+          return (
+            <>
+              <Header language="en" cta="Need assistance" flag={<England />} />
+              <Intro title="Select language" />
+              <LanguageOptions />
+              <Btn text="Back" />
+            </>
+          )
         } else if (body.classList.contains("de")) {
-          return <Header language="de" cta="Brauche Hilfe" flag={<German />} />
+          return (
+            <>
+              <Header language="de" cta="Brauche Hilfe" flag={<German />} />
+              <Intro title="Sprache auswählen" />
+              <LanguageOptions />
+              <Btn text="Abbrechen" />
+            </>
+          )
         } else if (body.classList.contains("fr")) {
-          return <Header language="fr" cta="Besoin d'assistance" flag={<France />} />
+          return (
+            <>
+              <Header language="fr" cta="Besoin d'assistance" flag={<France />} />
+              <Intro title="Choisir la langue" />
+              <LanguageOptions />
+              <Btn text="Retourner" />
+            </>
+          )
         } else {
-          return <Header language="es" cta="Necesita ayuda" flag={<Spain />} />
+          return (
+            <>
+              <Header language="es" cta="Necesita ayuda" flag={<Spain />} />
+              <Intro title="Seleccione el idioma" />
+              <LanguageOptions />
+              <Btn text="Regresa" />
+            </>
+          )
         }
       })()}
-      <main className="select-lang">
-        <Intro title="Select language" />
-        <LanguageOptions />
-        {(() => {
-          const body = document.querySelector("body")
-          if (body.classList.contains("en")) {
-            return <Btn text="Back" />
-          } else if (body.classList.contains("de")) {
-            return <Btn text="Abbrechen" />
-          } else if (body.classList.contains("fr")) {
-            return <Btn text="Retourner" />
-          } else {
-            return <Btn text="Regresa" />
-          }
-        })()}
-      </main>
-    </>
+    </main>
   )
 }
 
